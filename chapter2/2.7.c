3ac4afad41998d16d4d5bd4238e140cdee8b5841
Ankur Singh <ramanujanankur@gmail.com>
2018-11-24 16:16:15 +0800

    2.7.c

---
+++ chapter2/2.7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned invert(unsigned x, int p , int n);
+
+int main(){
+    unsigned x;
+    int p, n;
+    printf("Enter x: \n");
+    scanf("%d", &x);
+    printf("Enter p: \n");
+    scanf("%d", &p);
+    printf("Enter n: \n");
+    scanf("%d", &n);
+    printf("Answer is %d\n", invert(x, p, n));
+}
+
+
+unsigned invert(unsigned x, int p , int n){
+    return x ^ ~((~0 << (p + 1)) | ~(~0 << (p - n + 1)));
+}
