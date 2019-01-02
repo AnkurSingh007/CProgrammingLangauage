4135a55235720bcb71382124dd040d466f6fe913
Ankur Singh <ramanujanankur@gmail.com>
2019-01-02 16:48:04 +0800

    5.1.c

--- chapter5/5.1.c
+++ chapter5/5.1.c
@@ -15,7 +15,20 @@ int getint(int *pn){
 		ungetch(c);
 		return 0;
 	}
-	sign = (c == '-') ? -1 : 1
+	
+	sign = (c == '-') ? -1 : 1;
+	if(c == '+' || c == '-')
+		c = getch();
+	for(*pn = 0; isdigit(c) ; c = getch()){
+		*pn = 10 * *pn + (c - '0');
+	if(isdigit(c) && c == '+' || c == '-'){
+		ungetch(c);
+		return 0;
+	}
+	*pn *= sign;
+	if(c != EOF){
+		ungetch(c);
+	return c;
 }
 
 
