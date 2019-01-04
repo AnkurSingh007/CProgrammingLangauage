db4732bfd0c3a3da4a547c7836e30d790e441467
Ankur Singh <singh_a@worksap.co.jp>
2019-01-04 08:20:38 +0800

    5.5.c Added strncat

--- 5.5.c
+++ 5.5.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
 void strncpy2(char *s , char * t, int n);
+void strncat2(char *s , char *t , int n);
 
 char str1[100];
 char str2[100];
@@ -8,14 +9,26 @@ char str2[100];
 int main(){
 	printf("Enter the string\n");
 	scanf("%s", str1);
-	strncpy2(str2, str1, 5);
-	printf("after strncpy call str2 is %s\n", str2);	
+	printf("Enter the string\n");
+	scanf("%s", str2);
+	strncat2(str2, str1, 5);
+	printf("after strncat call str2 is %s\n", str2);
 }
 
 
 void strncpy2(char *s , char * t, int n){
 	int i = 0;
 	for(; *t != '\0' && i < n ; *s++ = *t++, i++)
+	;
+	*s = '\0';
+	return;
+}
+
+void strncat2(char *s , char *t, int n){
+	int i = 0;
+	for(; *s != '\0'; s++)
+	;
+	for(;i < n && *t != '\0'; *s++ = *t++, i++)
 	;
 	*s = '\0';
 	return;
